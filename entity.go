9571eb9102d73ca20dc9853b636478c605cbace9
Book-lab-final <kittisakbook.22@gmail.com>
2026-01-07 10:59:55 +0700

    First Commit & entity

---
+++ entity.go
@@ -0,0 +1,21 @@
+package sutfinallab
+
+import(
+	"gorm.io/gorm"
+	"github.com/asaskevich/govalidator"
+)
+
+type Book struct {
+	gorm.Model
+	Title	string		`valid:"stringlength(3|100)~Title Must long 3-100 character"`
+	Price	float64		`valid:"range(50|5000)~Price must be between 50 and 5000"`
+	Code	string		`valid:"matches(^BK[0-9]{6}$)"`
+}
+
+func (b *Book) ValidaterBook() error{
+	ok, err := govalidator.ValidateStruct(b)
+	if !ok {
+		return err
+	}
+	return nil
+}
